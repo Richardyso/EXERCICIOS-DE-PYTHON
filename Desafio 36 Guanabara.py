236308ca402be4c5b70cbd39479fe9c1a6217a9c
Richardyso <ri_chardyson@hotmail.com>
2023-04-27 15:45:00 -0300

    Exercício

--- Desafio 36 Guanabara.py
+++ Desafio 36 Guanabara.py
@@ -2,10 +2,11 @@ print('\033[44mTESTADOR CREDPAGO\033[m')
 nome = str(input('Qual seu nome: '))
 salario = int(input('Qual sua renda mensal, {}?'.format(nome)))
 valorcasa = int(input('Qual o valor do imóvel?'))
-parcela = int(input('Deseja parcelar em quantas vezes, {}?'.format(nome)))
-parcela1 = valorcasa/parcela
-
-if parcela1 >= salario*(salario*100/30):
+parcelas = int(input('Deseja parcelar em quantas vezes, {}?'.format(nome)))
+parceladoimovel = valorcasa/parcelas
+posso = salario*0.3
+if posso <= valorcasa*0.3:
     print('\033[31;107mO empréstimo para comprar o imóvel não foi aprovado!!\033[m')
 else:
-    print('Seu empréstimo está aprovado!/n Sua parcela será de:R$ {} por mês!'.format(parcela1))
+    print('Seu empréstimo está aprovado, sua parcela será de:R$ {:.2f} por mês!'.format(
+        parceladoimovel))
