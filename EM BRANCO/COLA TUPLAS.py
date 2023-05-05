331f61282f6a4b806972c7d67e3b35c78e03e731
Richardyso <ri_chardyson@hotmail.com>
2023-05-05 20:14:51 -0300

    TUPLA COLA

---
+++ EM BRANCO/COLA TUPLAS.py
@@ -0,0 +1,53 @@
+lanche = ('hamburguer', 'suco', 'pizza', 'pudim', 'batata frita')
+
+
+# TUPLAS CRUA COM ('','')
+# print(lanche)
+
+# TUPLAS ORGANIZADAS EM LISTA
+# print(sorted(lanche))
+
+# APENAS NOMES DAS TUPLAS
+# for c in (lanche):
+#     print(c)
+
+
+# APENAS NOME DA TUPLA2
+# for comida in (lanche):
+#     comida = lanche[2]
+# PARA TODOS EXCLUIR LINHA ACIMA
+# print(f'Eu comi {comida}')
+
+# POSIÇÃO MOSTRA A LETRA DO INDICE 2 DE CONT(2º ITEM DA LISTA)
+# cont = lanche[2]
+# print(f'Eu comi {cont} na posição {cont[2]}')
+
+
+# APENAS CONTAR QUANTOS TEM NA TUPLA
+# print(len(lanche))
+
+
+# INDICE DAS TUPLAS(posiçõesN)
+# for cont in range(0, len(lanche)):
+#     print(cont)
+
+# # TODAS TUPLAS SEM POSIÇÃO
+# for contador in lanche:
+#     print(f'Eu comi {contador}')
+# # OU MAIS CHATO DE LER
+# for contador in range(0, len(lanche)):
+#     print(f'Eu comi {lanche[contador]}')
+
+# TODAS TUPLAS COM POSIÇÕES
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu comi {comida} que esta na posição {pos}')
+# OU
+# for comida in range(0, len(lanche)):
+#     print(f'Eu comi {lanche[comida]} que esta na posição {comida}')
+
+
+# TUPLA()
+# LISTA[]
+# DICIONARIO{}
+# print(len(lanche))
+# print(lanche[0::3])
---
+++ EM BRANCO/Desafio 28 Guanabara - Copia (3).py
@@ -0,0 +1,2 @@
+a = ('dido', 28, 'm')
+print(f'Meu nome é {a[0]}, tenho {a[1]} anos e sou {a[2]}!')
