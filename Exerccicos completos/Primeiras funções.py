b0d706e0d0a619e217ade9a582ac54eb9d9bfc3a
Richardyso <ri_chardyson@hotmail.com>
2023-05-10 11:58:47 -0300

    Rename

--- Exerccicos completos/Primeiras funções.py
+++
@@ -1,8 +0,0 @@
-# import math
-# n=float(input('Enque com numero:'))
-# print('O numero inteiro é:{}!'.format (math.floor(n)))
-import random
-import math
-nome=['DIDO','FABRICIO','BERNARDO','VAGO']
-random.shuffle(nome)
-print('A ordem de apresentação sera:{}'.format(nome))
