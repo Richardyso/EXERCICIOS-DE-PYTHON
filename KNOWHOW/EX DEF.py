a74ac0ff4b6b2c306c3c86e8de917427cd26271e
Richardyso <ri_chardyson@hotmail.com>
2023-05-27 17:15:17 -0300

    up

--- KNOWHOW/EX DEF.py
+++ KNOWHOW/EX DEF.py
@@ -1,26 +1,36 @@
-# def tx():
-#     print('=*'*30)
+################################################ DEF BASICO
+# def linha():
+#     print('=='*15)
 
 
-# # principal
-# tx()
-# print('Ola mundo')
-# tx()
-############################################# OU #######################################################
-
-# def mensagem(tx):
-#     print('=*'*30)
-#     print(tx)
-#     print('=*'*30)
-
-
-# # principal
-# mensagem('Olá mundo')
-########################################################################################################
+# #PRINCIPAL
+# linha()
+# print('SISTEMA DE ALUNO'.center(30))
+# linha()
+# print('CADASTRO DE FUNCIONÁRIOS'.center(30))
+# linha()
+# print('ERROS DO SISTEMA'.center(30))
+# linha()
+################################################ DEF PARÂMETROS DEFINIDOS
+# def mensagem(msg):
+#     print('=='*15)
+#     print(msg.center(30))
+#     print('=='*15)
 
 
-# Desempacotar
-# def contador(*núm):
+# mensagem('SISTEMA DE ALUNO')
+# mensagem('CADASTRO DE FUNCIONÁRIOS')
+# mensagem('ERROS DO SISTEMA')
+################################################ DEF COM VALORES
+# def soma(a,b,c=0):
+#     d=a+b+c
+#     print(d)
+
+
+# soma(b=4,a=2)
+# soma(9,2)
+################################################ Empacotar
+# def contador(* núm):
 #     tam = len(núm)
 #     print(f'Recebi os valores {núm} e são {tam} números')
 #     print(' Fim')
@@ -29,8 +39,7 @@
 # contador(1, 2, 3)
 # contador(4, 5)
 # contador(6, 7, 8, 9)
-
-
+#####################################################################
 def dobra(lst):
     pos = 0
     while pos < len(lst):
